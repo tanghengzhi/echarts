57e02317644645394ec08ab660607d6859e5f968
Victor Tang <tanghengzhi@live.com>
2020-03-09 16:40:35 +0800

    echarts Map

---
+++ js/newmap.js
@@ -0,0 +1,117 @@
+var geoCoordMap = {
+    '糖果世界': [117.130387,34.235391],
+};
+
+var GZData = [
+    [{
+        name: '糖果世界'
+    }, {
+        name: '糖果世界',
+        value: 100
+    }],
+];
+
+var planePath = 'path://M.6,1318.313v-89.254l-319.9-221.799l0.073-208.063c0.521-84.662-26.629-121.796-63.961-121.491c-37.332-0.305-64.482,36.829-63.961,121.491l0.073,208.063l-319.9,221.799v89.254l330.343-157.288l12.238,241.308l-134.449,92.931l0.531,42.034l175.125-42.917l175.125,42.917l0.531-42.034l-134.449-92.931l12.238-241.308L1705';
+
+var convertData = function (data) {
+    var res = [];
+    for (var i = 0; i < data.length; i++) {
+        var dataItem = data[i];
+        var fromCoord = geoCoordMap[dataItem[0].name];
+        var toCoord = geoCoordMap[dataItem[1].name];
+        if (fromCoord && toCoord) {
+            res.push([{
+                coord: fromCoord
+            }, {
+                coord: toCoord
+            }]);
+        }
+    }
+    return res;
+};
+
+var color = ['#3ed4ff', '#ffa022', '#a6c84c'];
+var series = [];
+[
+    ['新疆', GZData]
+].forEach(function (item, i) {
+    series.push({
+        name: item[0] + ' Top10',
+        type: 'effectScatter',
+        coordinateSystem: 'geo',
+        zlevel: 2,
+        rippleEffect: {
+            brushType: 'stroke'
+        },
+        label: {
+            normal: {
+                show: true,
+                position: 'right',
+                formatter: '{b}'
+            }
+        },
+        symbolSize: function (val) {
+            return val[2] / 8;
+        },
+        itemStyle: {
+            normal: {
+                color: color[i]
+            }
+        },
+        data: item[1].map(function (dataItem) {
+            return {
+                name: dataItem[1].name,
+                value: geoCoordMap[dataItem[1].name].concat([dataItem[1].value])
+            };
+        })
+    });
+});
+
+option = {
+    backgroundColor: '#080a20',
+    title: {
+        left: 'left',
+        textStyle: {
+            color: '#fff'
+        }
+    },
+    tooltip: {
+        trigger: 'item'
+    },
+    legend: {
+        orient: 'vertical',
+        top: 'bottom',
+        left: 'right',
+        data: ['北京 Top10', '上海 Top10', '广州 Top10'],
+        textStyle: {
+            color: '#fff'
+        },
+        selectedMode: 'single'
+    },
+    geo: {
+        map: 'park',
+        zoom: 1.2,
+        label: {
+            emphasis: {
+                show: false
+            }
+        },
+        roam: true,
+        itemStyle: {
+            normal: {
+                areaColor: '#142957',
+                borderColor: '#0692a4'
+            },
+            emphasis: {
+                areaColor: '#0b1c2d'
+            }
+        }
+    },
+    series: series
+};
+
+$.get('data/park.json', function (json) {
+    echarts.registerMap('park', json);
+    var myecharts = echarts.init($('.map .geo')[0])
+    myecharts.setOption(option)
+})
